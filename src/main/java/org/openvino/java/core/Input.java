8b10d114190e0409ba72f73ac604b60794baa1fc
Ming Huang <444308640@qq.com>
2023-10-28 22:01:28 +0800

    增加input/output 函数

--- src/main/java/org/openvino/java/core/Input.java
+++ src/main/java/org/openvino/java/core/Input.java
@@ -16,28 +16,72 @@ public class Input extends OpenVINOCls {
      */
     private Node mNode;
 
+    /**
+     *
+     */
     private long index;
 
+    /**
+     * Constructs a input.
+     * @param node The node for the input handle.
+     * @param index The index of the input.
+     */
     public Input(Node node,long index) {
         super("Input-" + index,null);
         this.mNode = node;
         this.index = index;
     }
 
+    /**
+     * Any tensor names associated with this input
+     * @return tensor names<
+     */
     public String getAnyName() {
         return mNode.getName();
     }
 
+    /**
+     * The element type of the input referred to by this input handle.
+     * @return The element type of the input.
+     */
     public int getElementType() {
         return mNode.getElementType();
     }
 
+    /**
+     * The shape of the input referred to by this input handle.
+     * @return The shape of the input .
+     */
     public Shape getShape() {
         return mNode.getShape();
     }
 
+    /**
+     * Get the node referred to by this input handle.
+     * @return The ouput node
+     */
+    public Node getNode() {
+        return mNode;
+    }
+
+    /**
+     * The index of the input referred to by this input handle.
+     * @return The index of the input.
+     */
+    public long getIndex() {
+        return index;
+    }
+
+    /**
+     * The partial shape of the input referred to by this input handle.
+     * @return The partial shape of the input
+     */
+    public PartialShape getPartialShape() {
+        return mNode.getPartialShape();
+    }
+
     @Override
     protected void dispose() {
-
+        mNode.dispose();
     }
 }
--- src/main/java/org/openvino/java/core/Output.java
+++ src/main/java/org/openvino/java/core/Output.java
@@ -2,36 +2,82 @@ package org.openvino.java.core;
 
 import org.openvino.java.base.OpenVINOCls;
 
+/**
+ *  A handle for one of a node's outputs.
+ */
 public class Output extends OpenVINOCls {
 
+    /**
+     *
+     */
     private Node mNode;
 
+    /**
+     *
+     */
     private long index;
 
+    /**
+     * Constructs a Output.
+     * @param node The node for the output handle.
+     * @param index The index of the output.
+     */
     public Output(Node node, long index) {
         super("Output-" + index,null);
         this.mNode = node;
         this.index = index;
     }
 
+    /**
+     * Any tensor names associated with this input
+     * @return tensor names<
+     */
     public String getAnyName() {
         return mNode.getName();
     }
 
+    /**
+     * The element type of the input referred to by this input handle.
+     * @return The element type of the input.
+     */
     public int getElementType() {
         return mNode.getElementType();
     }
 
+    /**
+     * The shape of the input referred to by this input handle.
+     * @return The shape of the input .
+     */
     public Shape getShape() {
         return mNode.getShape();
     }
 
-    @Override
-    protected void dispose() {
-
-    }
-
+    /**
+     * Get the node referred to by this input handle.
+     * @return The ouput node
+     */
     public Node getNode() {
         return mNode;
+    }
+
+    /**
+     * The index of the input referred to by this input handle.
+     * @return The index of the input.
+     */
+    public long getIndex() {
+        return index;
+    }
+
+    /**
+     * The partial shape of the input referred to by this input handle.
+     * @return The partial shape of the input
+     */
+    public PartialShape getPartialShape() {
+        return mNode.getPartialShape();
+    }
+
+    @Override
+    protected void dispose() {
+        mNode.dispose();
     }
 }
